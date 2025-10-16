9f1c11780049119655e778b9ac54ba5706f2a7a3
Tope Bello <Belltop12@gmail.com>
2025-10-16 15:19:09 +0100

    socket

--- frontend/src/pages/UserInbox.jsx
+++ frontend/src/pages/UserInbox.jsx
@@ -9,7 +9,7 @@ import { useNavigate } from "react-router-dom";
 import { AiOutlineArrowRight, AiOutlineSend } from "react-icons/ai";
 import { TfiGallery } from "react-icons/tfi";
 import styles from "../styles/styles";
-// import socket from "../socket";
+import socket from "../socket";
 import socketIO from "socket.io-client";
 const ENDPOINT = "https://eshopx-socket-server.onrender.com/";
 const socketId = socketIO(ENDPOINT, { transports: ["websocket"] });
