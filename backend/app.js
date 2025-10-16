d68edd7744b603b34c9100886001706448d206a4
Tope Bello <Belltop12@gmail.com>
2025-10-16 12:43:17 +0100

    paths

--- backend/app.js
+++ backend/app.js
@@ -6,7 +6,7 @@ const bodyParser = require("body-parser");
 const cors = require("cors");
 
 app.use(cors({
-  origin: ['https://eshopx-live.vercel.app/',
+  origin: ['https://eshopx-live.vercel.app',
      'http://localhost:3000' // local development
   ],
   
@@ -15,7 +15,7 @@ app.use(cors({
 
 app.use(express.json({ limit: "50mb" }));
 app.use(cookieParser());
-app.use("/", (req, res) => {
+app.use("/test", (req, res) => {
   res.send("Hello world!");
 });
 
